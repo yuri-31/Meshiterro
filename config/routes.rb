0f930d31f3ec43efc60958e3f6c7016fd3263492
YuriNakano <yuriinlethbridge@outlook.com>
2022-08-07 23:45:36 +0000

    [Finish] 4-13

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
