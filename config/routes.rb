ad9989faffa279120893d34f729505e27d34554e
YuriNakano <yuriinlethbridge@outlook.com>
2022-08-08 19:49:28 +0000

    [Finish] 4-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: "homes#top"
 
@@ -6,6 +7,10 @@ Rails.application.routes.draw do
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
+  
+  resources :users, only: [:show, :edit]
+  # get 'users/show'
+  # get 'users/edit'
   
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
